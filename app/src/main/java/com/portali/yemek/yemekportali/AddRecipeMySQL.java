ff4b533bb77756cbaf89fd399bce684df4cf4448
gurko32 <gurko32@gmail.com>
2016-12-31 21:32:33 +0300

    Devamı gelcekte foreign key hata veriyo anlamadım

--- app/src/main/java/com/portali/yemek/yemekportali/AddRecipeMySQL.java
+++ app/src/main/java/com/portali/yemek/yemekportali/AddRecipeMySQL.java
@@ -3,6 +3,7 @@ package com.portali.yemek.yemekportali;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class AddRecipeMySQL extends AppCompatActivity {
 
@@ -13,14 +14,24 @@ public class AddRecipeMySQL extends AppCompatActivity {
     }
 
     public void addRecipe(View view) {
-//        String str_name=editText1.getText().toString();
-//        String str_password=editText2.getText().toString();
-//        String str_mail=editText3.getText().toString();
-//        int id=str_name.hashCode();
-//        String idReal=id+"";
-//        String type="addMeal";
-//
-//        OnlineConnection onlineConnection=new OnlineConnection(this);
-//        onlineConnection.execute(type,str_name,str_password,str_mail);
+        EditText editText1=(EditText)findViewById(R.id.newName);
+        EditText editText2=(EditText)findViewById(R.id.newDesc);
+        EditText editText3=(EditText)findViewById(R.id.newTime);
+        EditText editText4=(EditText)findViewById(R.id.rating);
+        EditText editText5=(EditText)findViewById(R.id.newIng);
+        EditText editText6=(EditText)findViewById(R.id.type);
+
+        String name=editText1.getText().toString();
+        String recipe=editText2.getText().toString();
+        String time=editText3.getText().toString();
+        String rating=editText4.getText().toString();
+        String ingredients=editText5.getText().toString();
+        String typeM=editText6.getText().toString();
+
+
+        String type="addMeal";
+
+        OnlineConnection onlineConnection=new OnlineConnection(this);
+        onlineConnection.execute(type,name,recipe,time,rating,ingredients,typeM);
     }
 }
--- app/src/main/java/com/portali/yemek/yemekportali/OnlineConnection.java
+++ app/src/main/java/com/portali/yemek/yemekportali/OnlineConnection.java
@@ -38,6 +38,7 @@ public class OnlineConnection extends AsyncTask<String,Void,String> {
         String type= params[0];
         String login_url="http://10.0.2.2/login.php";
         String register_url="http://10.0.2.2/register.php";
+        String addMeal_url="http://10.0.2.2/addMeal.php";
         if(type.equals("login")){
             try {
                 String user_name= params[1];
@@ -92,6 +93,58 @@ public class OnlineConnection extends AsyncTask<String,Void,String> {
                         +URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+"&"
                         +URLEncoder.encode("user_password","UTF-8")+"="+URLEncoder.encode(user_password,"UTF-8")+"&"
                         +URLEncoder.encode("user_mail","UTF-8")+"="+URLEncoder.encode(user_mail,"UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream=httpURLConnection.getInputStream();
+                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result="";
+                String line="";
+                while((line=bufferedReader.readLine())!=null){
+
+                    result+=line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+
+
+
+                return result;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else if(type.equals("addMeal")){
+            try {
+                String name= params[1];
+                String recipe= params[2];
+                String time= params[3];
+                String rating= params[4];
+                String ingredients= params[5];
+                String typeM= params[6];
+                mid=Math.abs(name.hashCode());
+
+                URL url=new URL(addMeal_url);
+                HttpURLConnection httpURLConnection=(HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream=httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter=new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+                String post_data=
+                        URLEncoder.encode("meal_id","UTF-8")+"="+URLEncoder.encode(Integer.toString(mid),"UTF-8")+"&"
+                                +URLEncoder.encode("meal_name","UTF-8")+"="+URLEncoder.encode(name,"UTF-8")+"&"
+                                +URLEncoder.encode("meal_recipe","UTF-8")+"="+URLEncoder.encode(recipe,"UTF-8")+"&"
+                                +URLEncoder.encode("meal_time","UTF-8")+"="+URLEncoder.encode(time,"UTF-8")+"&"
+                                +URLEncoder.encode("meal_rating","UTF-8")+"="+URLEncoder.encode(rating,"UTF-8")+"&"
+                                +URLEncoder.encode("meal_ingredients","UTF-8")+"="+URLEncoder.encode(ingredients,"UTF-8")+"&"
+                                +URLEncoder.encode("meal_type","UTF-8")+"="+URLEncoder.encode(typeM,"UTF-8")+"&"
+                                +URLEncoder.encode("meal_userid","UTF-8")+"="+URLEncoder.encode(Integer.toString(uid),"UTF-8");
                 bufferedWriter.write(post_data);
                 bufferedWriter.flush();
                 bufferedWriter.close();
